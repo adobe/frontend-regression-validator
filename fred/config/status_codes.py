0c729bd2dd683994da402417109137848db90f56
Alexandru Meterez <meterez@motherland.corp.adobe.com>
2019-09-05 17:34:55 +0300

    Add config containing status codes and other constants

---
+++ fred/config/status_codes.py
@@ -0,0 +1,7 @@
+STATUS_CODES = {
+    1: 'Invalid website',
+    2: 'Prediction script failed',
+    3: 'Failed to launch inference process',
+    4: 'Model does not exist',
+    5: 'CHROMEDRIVER_PATH not provided in env variables'
+}
